4853a17ed44314276769b19cf741192709e31575
AaronJack <ajack9601@bths.edu>
2018-11-29 11:45:06 -0500

    nothing

--- src/com/company/QuickSort.java
+++ src/com/company/QuickSort.java
@@ -7,7 +7,7 @@ public class QuickSort {
         int holder = 0;
         for(int j = 0; j < arr.length; j++){
             if(arr[j] <= pivot){
-                i = i +1;
+                i++;
                 arr[i] = holder;
                 arr[i] = arr[j];
                 arr[j] = holder;
@@ -15,14 +15,37 @@ public class QuickSort {
         }
         arr[right] = holder;
         arr[right] = arr[i + 1];
-        arr[i = 1] = holder;
+        arr[i + 1] = holder;
         return i+1;
     }
     public static void quickSort(int[] arr, int left, int right){
-        if(left < right){
-            int pivot = partition(arr, left, right);
-            quickSort(arr, left, pivot - 1);
-            quickSort(arr, pivot + 1, right);
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        int pivot = partition(arr, left, right);
+        int i = left;
+        int j = right;
+        while( i <= j){
+            while(arr[i] < pivot){
+                i++;
+            }
+            while(arr[j] > pivot){
+                j--;
+            }
+            if(i <= j){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        if(left < j){
+            quickSort(arr, left, right);
+        }
+        if(right > 1){
+            quickSort(arr, i, right);
         }
     }
 }
